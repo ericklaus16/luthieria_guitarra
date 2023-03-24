68f27f77c94fecac859a4b41b0b9c571af2b0d36
Eric Klaus <ericklaus16@gmail.com>
2023-03-24 11:24:44 -0300

    Arquivos headers adicionados

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "montar.h"
+#include "comprar.h"
+#include "vender.h"
 
 struct Usuario {
     char email[50];
@@ -10,4 +13,22 @@ void logar(){
     printf("Bem-vindo ao sistema da melhor loja de guitarras do mundo!\n");
     printf("[1] Cadastro de Usuario\n");
     printf("[2] Login\n");
+
+    montarLoja();
+}
+
+void montarLoja(){
+    int opcao;
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+
+    switch(opcao){
+        case 1:
+            montar();
+            break;
+        case 2:
+            break;
+        default:
+            montarLoja();
+    }
 }
